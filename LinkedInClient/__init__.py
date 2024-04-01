dfff5cbcc601a409518a31454a11dd59b97f6781
Mirci212 <Marcomaier1006@outlook.com>
2024-04-01 22:52:05 +0200

    Add LinkedInClient getAccessToken

--- LinkedInClient/__init__.py
+++ LinkedInClient/__init__.py
@@ -1,3 +1,26 @@
+# System imports
+import requests
+
+
 class LinkedInClient:
-    def __init__(self):
-        return
+    _clientId: str
+    _clientSecret: str
+    OAUTHADDRESS = 'https://www.linkedin.com/oauth/v2'
+
+    def __init__(self, clientId: str, clientSecret: str):
+        self._clientId = clientId
+        self._clientSecret = clientSecret
+        self.result = self._getAccessToken(clientId,clientSecret)
+        print()
+
+    def _getAccessToken(self, clientId: str, clientSecret: str):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': clientId,
+            'client_secret': clientSecret
+        }
+        return requests.post(url=self.OAUTHADDRESS + "/accessToken",headers=headers,data=data)
+
